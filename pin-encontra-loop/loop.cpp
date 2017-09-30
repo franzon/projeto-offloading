e496909cc492edac6ae853651d111174f4a5a67c
Jorge Rossi <jorgefzrossi@gmail.com>
2017-09-30 18:27:54 -0300

    Dúvida loops

--- pin-encontra-loop/loop.cpp
+++ pin-encontra-loop/loop.cpp
@@ -8,29 +8,36 @@ unsigned long trace_count = 0;
 
 void Routine(RTN rtn, VOID *v)
 {
+    // Descartar as outras funções para teste
     if (RTN_Name(rtn).compare("main") == 0)
     {
         RTN_Open(rtn);
+
+        // Percorre as instruções
         for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
         {
             TraceFile << std::hex << INS_Address(ins) << " : " << INS_Disassemble(ins);
+
+            // Se for uma instrução do tipo branch condicional  
             if (INS_Category(ins) == XED_CATEGORY_COND_BR)
             {
                 unsigned long target = INS_DirectBranchOrCallTargetAddress(ins);
                 bool found_target = false;
+
+                // Certifica que existe o destino do branch.
                 for (INS target_ins = ins; INS_Valid(target_ins); target_ins = INS_Prev(target_ins))
                 {
-
                     if (INS_Address(target_ins) == target)
                     {
                         found_target = true;
                         break;
                     }
                 }
+
                 if (found_target)
                 {
                     TraceFile
-                        << " -- HERE -- Target: " << std::hex << target;
+                            << " -- HERE -- Target: " << std::hex << target;
                 }
             }
             TraceFile << endl;
