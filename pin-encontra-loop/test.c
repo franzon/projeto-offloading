db112807aa06aa7f968ec92341ef8035bba91ed5
fjorg <jorgefzrossi@gmail.com>
2017-11-20 16:23:03 -0200

    analise gcc

--- pin-encontra-loop/test.c
+++ pin-encontra-loop/test.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct el {
+    int val;
+    struct el* next;
+} El;
+
+typedef struct list {
+    El* first;
+} LinkedList;
 
 int main()
 {
 
-	for (int i = 0; i < 10; i++)
-	{
-		printf("X");
-	}
+    LinkedList* l = (LinkedList*)malloc(sizeof(LinkedList));
 
-	return 0;
+    for (El* tmp = l->first; tmp != NULL; tmp = tmp->next)
+        printf("%d ", tmp->val);
+
+    return 0;
 }
