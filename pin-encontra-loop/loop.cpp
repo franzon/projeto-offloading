8c715c6cfbc784e4a5ea9dac8612b50b64b7ec8e
Jorge Rossi <jorgefzrossi@gmail.com>
2017-11-27 09:43:15 -0200

    agora vai

--- pin-encontra-loop/loop.cpp
+++ pin-encontra-loop/loop.cpp
@@ -6,54 +6,80 @@
 std::ofstream TraceFile;
 unsigned long trace_count = 0;
 
-void Routine(RTN rtn, VOID *v)
-{
-    // Descartar as outras funções para teste
-    if (RTN_Name(rtn).compare("main") == 0)
-    {
-        RTN_Open(rtn);
+// todo: melhorar esse código
+int isLoopSig(INS ins) {
+    if (INS_IsNop(ins)) {
+        INS n1 = INS_Next(ins);
 
-        // Percorre as instruções
-        for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
-        {
-            TraceFile << std::hex << INS_Address(ins) << " : " << INS_Disassemble(ins);
+        if (INS_Valid(n1)) {
+            INS n2 = INS_Next(n1);
+            if (INS_Valid(n2)) {
+                INS n3 = INS_Next(n2);
+                if (INS_Valid(n3)) {
+                    if (INS_IsMov(n1) && INS_OperandCount(n1) == 2) {
+                        if (INS_OperandIsReg(n1, 0) && INS_OperandIsReg(n1, 1) &&
+                            INS_OperandReg(n1, 0) == INS_OperandReg(n1, 1)) {
 
-            // Se for uma instrução do tipo branch condicional  
-            if (INS_Category(ins) == XED_CATEGORY_COND_BR)
-            {
-                unsigned long target = INS_DirectBranchOrCallTargetAddress(ins);
-                bool found_target = false;
+                            if (INS_IsMov(n2) && INS_OperandCount(n2) == 2) {
+                                if (INS_OperandIsReg(n2, 0) && INS_OperandIsReg(n2, 1) &&
+                                    INS_OperandReg(n2, 0) == INS_OperandReg(n2, 1)) {
+                                    if (INS_IsNop(n3))
+                                        if (INS_Valid(INS_Next(n3)))
+                                            return INS_Address(INS_Next(n3));
+                                }
+                            }
+                        }
 
-                // Certifica que existe o destino do branch.
-                for (INS target_ins = ins; INS_Valid(target_ins); target_ins = INS_Prev(target_ins))
-                {
-                    if (INS_Address(target_ins) == target)
-                    {
-                        found_target = true;
-                        break;
                     }
                 }
-
-                if (found_target)
-                {
-                    TraceFile
-                            << " -- HERE -- Target: " << std::hex << target;
-                }
             }
-            TraceFile << endl;
         }
+
+
+    }
+    return 0;
+}
+
+void Routine(RTN rtn, VOID *v) {
+    if (RTN_Valid(rtn) && RTN_Name(rtn).compare("loop_signature") != 0) {
+        RTN_Open(rtn);
+        int is_loop;
+        int print_fn = 0;
+        for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
+
+
+            is_loop = isLoopSig(ins);
+            if (is_loop) {
+                print_fn = 1;
+                TraceFile << "Loop start: " << std::hex << is_loop << endl;
+            }
+
+            // INS_IsMovFullRegRegSame não está funcionando
+        }
+
+
+        if (print_fn) {
+
+            for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
+                TraceFile << INS_Address(ins) << ": " << INS_Disassemble(ins);
+                TraceFile << endl;
+
+            }
+
+            TraceFile << endl << endl;
+        }
+
         RTN_Close(rtn);
+
     }
 }
 
-VOID Fini(INT32 code, VOID *v)
-{
+VOID Fini(INT32 code, VOID *v) {
 
     TraceFile.close();
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     PIN_InitSymbols();
     PIN_Init(argc, argv);
 
--- pin-encontra-loop/test.c
+++ pin-encontra-loop/test.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct el {
-    int val;
-    struct el* next;
-} El;
 
-typedef struct list {
-    El* first;
-} LinkedList;
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+__attribute__((always_inline)) void loop_signature()
+{
+    asm volatile("jmp 0f;");
+    asm volatile("nop");
+    asm volatile("mov %eax, %eax");
+    asm volatile("mov %ebx, %ebx");
+    asm volatile("nop");
+  asm volatile("0:");
+}
+#pragma GCC pop_options
+
 
 int main()
 {
-
-    LinkedList* l = (LinkedList*)malloc(sizeof(LinkedList));
-
-    for (El* tmp = l->first; tmp != NULL; tmp = tmp->next)
-        printf("%d ", tmp->val);
-
+    loop_signature();
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%d\n", i);
+    }
     return 0;
 }
